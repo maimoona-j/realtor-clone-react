3e00e8f1e66c7d315527f53b047183f556c20c09
maimoona <maimoonaj97@gmail.com>
2023-05-02 21:34:33 +0500

    complete the oauth functionality

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -20,10 +20,10 @@ function onchange(e){
 
   return (
     <section>
-  <h1 className='text-3x text-center mt-6 font-bold'>Sign In</h1>
+  <h1 className='text-3xl text-center mt-6 font-bold'>Sign In</h1>
    <div className='flex justify-center flex-wrap items-center px-6 py-12 max-w-6xl mx-auto'>
     <div className='md: w-[67%] lg: w-[50%] mb-12 md:mb-6'>
-        <img src="https://images.unsplash.com/flagged/photo-1564767609342-620cb19b2357?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8a2V5fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=1000&q=60" alt="key"  className='w-full rounded-2x1'/>
+        <img src="https://images.unsplash.com/flagged/photo-1564767609342-620cb19b2357?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8a2V5fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=1000&q=60" alt="key"  className='w-full rounded-2xl'/>
     </div>
 
     <div>
@@ -31,7 +31,7 @@ function onchange(e){
     <input type="email"   id='email' value={email} onChange={onchange} placeholder="Email address" className="mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out"/>
 
     <div className='relative mb-6'>
-    <input type= {showPassword ? "text": "password" }  id='password' value={email} onChange={onchange} placeholder="password" className="w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out"/> 
+    <input type= {showPassword ? "text": "password" }  id='password' value={password} onChange={onchange} placeholder="password" className="w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out"/> 
 
     {showPassword ?(
        <AiFillEyeInvisible className='absolute right-3 top-3 text-xl cursor-pointer' onClick={()=>setShowPassword((prevState)=>!prevState)}  />
