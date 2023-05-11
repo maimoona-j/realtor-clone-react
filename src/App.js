d4e509be83c2d4c0a8f2e3856fca5a266eb061d8
maimoona <maimoonaj97@gmail.com>
2023-05-11 22:15:34 +0500

    profile update

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
-import privateRoute from "./Components/privateRoute";
+import PrivateRoute from "./Components/PrivateRoute";
 import Offers from "./pages/Offers";
 import ForgotPassword from "./pages/ForgotPassword";
 import Header from "./Components/Header";
@@ -18,9 +18,9 @@ import 'react-toastify/dist/ReactToastify.css';
         <Header/>
         <Routes>
 <Route path="/" element={<Home/>} /> 
-<Route path ="/profile" element ={<privateRoute/>}>
-<Route path="/profile" element={<Profile/>} /> 
-</Route>
+<Route path="/profile" element={<PrivateRoute />}>
+            <Route path="/profile" element={<Profile />} />
+          </Route>
 
 <Route path="/sign-in" element={<SignIn/>} /> 
 <Route path="/sign-up" element={<SignUp/>} /> 
--- src/hooks/useAuthStatus.jsx
+++ src/hooks/useAuthStatus.jsx
@@ -1,22 +1,19 @@
-import { getAuth, onAuthStateChanged } from 'firebase/auth'
-import React from 'react'
-import { useEffect } from 'react'
-import { useState } from 'react'
+import { useEffect, useState } from "react";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
-export  function useAuthStatus() {
-    const [loggedIn, setLoggedIn] = useState(false)
-    const [checkingStatus, setCheckingStatus]=useState(true)
-  
-    useEffect(()=>{
-        const auth = getAuth()
-        onAuthStateChanged (auth, (user)=>{
-            if(user){
-               setLoggedIn(true) 
-            }
-            setCheckingStatus(false)
-        })
-    },[])
+export function useAuthStatus() {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [checkingStatus, setCheckingStatus] = useState(true);
 
-
-    return {loggedIn, checkingStatus};
+  useEffect(() => {
+    const auth = getAuth();
+    console.log(auth);
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setLoggedIn(true);
+      }
+      setCheckingStatus(false);
+    });
+  }, []);
+  return { loggedIn, checkingStatus };
 }
